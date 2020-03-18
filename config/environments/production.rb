306318cd5cb957dc281225a3643d549d9deef27a
doubleInc <aalavi@protonmail.com>
2020-03-18 21:21:46 +1100

    Make changes for heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Heroku settings update
+  config.assets.digest = true
+  config.assets.compile = true
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
